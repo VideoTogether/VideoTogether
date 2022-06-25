b25811bdd1d7e0a8d44f7225ffb0396ac6f929a4
Weijie Zhao <maggch@outlook.com>
2022-06-25 14:34:49 +0800

    Add extension

--- source/js/extension.js
+++ source/js/extension.js
@@ -1,10 +1,15 @@
 // ==UserScript==
-// @name         Video Together
+// @name         Video Together 一起看视频
 // @namespace    http://vt.panghair.com
 // @version      0.1
-// @description  Watch video together / 一起看视频
+// @description  Watch video together 一起看视频
 // @author       maggch@outlook.com
-// @match       *://*/*
-// @icon         https://www.google.com/s2/favicons?sz=64&domain=tampermonkey.net
+// @match        *://*/*
+// @icon         https://cdn.jsdelivr.net/gh/maggch97/VideoTogether/icon/favicon-32x32.png
 // @grant        none
 // ==/UserScript==
+
+var script = document.createElement('script');
+script.type = 'text/javascript';
+script.src = 'https://cdn.jsdelivr.net/gh/maggch97/VideoTogether/source/js/vt.js';
+document.getElementsByTagName('body')[0].appendChild(script);
