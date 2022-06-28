d3cc413c29f9fa1233ede5d01cdeec8071d2af7e
Weijie Zhao <maggch@outlook.com>
2022-06-28 22:25:08 +0800

    add loading

--- release/extension.user.js
+++ release/extension.user.js
@@ -13,7 +13,36 @@
 
 (function () {
     let wrapper = document.createElement("div");
-    wrapper.innerHTML = `{{{ {"user": "./html/loading.html"} }}}`
+    wrapper.innerHTML = `<div id="videoTogetherLoading">
+    <div style="width: 100%">
+        <img style="display: inline;" src="https://cdn.jsdelivr.net/gh/maggch97/VideoTogether/icon/favicon-16x16.png">
+        <a target="_blank" href="https://github.com/maggch97/VideoTogether/blob/main/README.MD" style="display: inline;color: black;">Video Together 加载中...</p>
+    </div>
+</div>
+
+<style>
+    #videoTogetherLoading {
+        line-height: 16px;
+        height: 80px;
+        font-size: 16px;
+        border: solid;
+        border-width: 2px;
+        border-bottom-color: #424242;
+        border-right-color: #424242;
+        border-left-color: #fff;
+        border-top-color: #fff;
+        background: silver;
+        color: #212529;
+        display: flex;
+        align-items: center;
+        z-index: 2147483646;
+        position: fixed;
+        bottom: 15px;
+        right: 15px;
+        width: 250px;
+        text-align: center;
+    }
+</style>`
     document.getElementsByTagName('body')[0].appendChild(wrapper);
     var script = document.createElement('script');
     script.type = 'text/javascript';
--- source/js/tampermonkey/extension.user.js
+++ source/js/tampermonkey/extension.user.js
@@ -10,8 +10,10 @@
 // @updateURL    https://raw.githubusercontent.com/maggch97/VideoTogether/main/source/js/tampermonkey/extension.user.js
 // @grant        none
 // ==/UserScript==
-
-var script = document.createElement('script');
-script.type = 'text/javascript';
-script.src = 'https://cdn.jsdelivr.net/gh/maggch97/VideoTogether@latest/source/js/vt.js';
-document.getElementsByTagName('body')[0].appendChild(script);
+(function(){
+    var loading
+    var script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.src = 'https://cdn.jsdelivr.net/gh/maggch97/VideoTogether@latest/source/js/vt.js';
+    document.getElementsByTagName('body')[0].appendChild(script);
+})();
