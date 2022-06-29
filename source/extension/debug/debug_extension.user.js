94e0f043df540fd679d38b4603d19f5b79c5e7a2
Weijie Zhao <maggch@outlook.com>
2022-06-30 00:06:39 +0800

    add debug user script

---
+++ source/extension/debug/debug_extension.user.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name         Video Together 一起看视频
+// @namespace    http://vt.panghair.com
+// @version      0.1
+// @description  Watch video together 一起看视频
+// @author       maggch@outlook.com
+// @match        *://*/*
+// @icon         https://cdn.jsdelivr.net/gh/maggch97/VideoTogether/icon/favicon-32x32.png
+// @grant        none
+// @require      http://127.0.0.1:8000/release/vt.debug.user.js
+// ==/UserScript==
