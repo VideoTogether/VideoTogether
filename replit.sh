294bd54e0e61662197d8ba3897ac38d990d68c44
valetzx <45054400+valetzx@users.noreply.github.com>
2022-07-11 15:56:57 +0800

    Update replit.sh

--- replit.sh
+++ replit.sh
@@ -1,7 +1,6 @@
 host=https:\/\/$REPL_ID.id.repl.co
 common=https:\/\/vt.panghair.com:5000\/
 
-
 sed -i "s#$common#$host#g" ./source/extension/config/release_host
 python ./script/build_extension.py
 cp ./release/vt.user.js .
