0e6477f9b57b5a19bdc855f1d2f19a42b4c5d49d
Weijie Zhao <maggch@outlook.com>
2022-07-14 19:39:00 +0800

    fix test

--- test/extension/sync.js
+++ test/extension/sync.js
@@ -44,6 +44,7 @@ const findChrome = require('carlo/lib/find_chrome');
 
         await pageMember1.goto('https://www.baidu.com/');
         await sleep(1000);
+        await pageMember1.$eval('#videoTogetherRoomNameInput', el => el.value = '');
         const MNameInput = await pageMember1.$("#videoTogetherRoomNameInput");
         await MNameInput.type(roomName);
         const videoTogetherJoinButton = await pageMember1.$("#videoTogetherJoinButton");
@@ -63,7 +64,7 @@ const findChrome = require('carlo/lib/find_chrome');
             await sleep(5000);
             await pageMember1.screenshot({ path: name + "_" + (++x) + '_member.png', fullPage: false });
             await pageHost.screenshot({ path: name + "_" + (x) + '_host.png', fullPage: false });
-            
+
             if (i % 4 == 0) {
                 await pageHost.keyboard.press("Space");
             }
@@ -72,7 +73,7 @@ const findChrome = require('carlo/lib/find_chrome');
     }
     // !!!!!!!!! we need to fix memeber sync error when room video info is undefined
     await Test("https://v.qq.com/x/cover/mzc00200syg68ky/n33304xp9i6.html", "tencent", 5 * 1000);
-    await Test("https://www.bilibili.com/video/BV1nW4y1S7Fi?spm_id_from=333.851.b_7265636f6d6d656e64.7", "bilibili",5*1000);
+    await Test("https://www.bilibili.com/video/BV1nW4y1S7Fi?spm_id_from=333.851.b_7265636f6d6d656e64.7", "bilibili", 5 * 1000);
 
     await browser.close();
 })();
