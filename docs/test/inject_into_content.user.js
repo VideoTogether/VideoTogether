334a8c18d21595485decf2d9950771a3eb0d5985
Weijie Zhao <maggch@outlook.com>
2022-07-30 16:17:52 +0800

    add inline js test

---
+++ docs/test/inject_into_content.user.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name         test inject into content
+// @namespace    https://2gether.video/
+// @version      1658939040
+// @description  test inject into content
+// @author       maggch@outlook.com
+// @match        *://*/*
+// @icon         https://2gether.video/icon/favicon-32x32.png
+// @grant        none
+// @inject-into  content
+// ==/UserScript==
+
+alert("content");
