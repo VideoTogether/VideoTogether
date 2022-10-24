f9b62898c0c5f079ced751f8fc784fbd408e9496
Weijie Zhao <maggch@outlook.com>
2022-10-24 23:21:08 +0800

    go service update & get api

--- source/go-server/main.go
+++ source/go-server/main.go
@@ -2,23 +2,27 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"math"
 	"net/http"
 	"os"
+	"strconv"
 	"strings"
 	"sync"
+	"time"
 
 	"github.com/unrolled/render"
 )
 
 type PublicRoom struct {
 	Name                 string  `json:"name"`
-	LastUpdateClientTime float32 `json:"lastUpdateClientTime"`
-	LastUpdateServerTime float32 `json:"lastUpdateServerTime"`
-	PlaybackRate         float32 `json:"playbackRate"`
-	CurrentTime          float32 `json:"currentTime"`
+	LastUpdateClientTime float64 `json:"lastUpdateClientTime"`
+	LastUpdateServerTime float64 `json:"lastUpdateServerTime"`
+	PlaybackRate         float64 `json:"playbackRate"`
+	CurrentTime          float64 `json:"currentTime"`
 	Paused               bool    `json:"paused"`
 	Url                  string  `json:"url"`
-	Duration             float32 `json:"duration"`
+	Duration             float64 `json:"duration"`
 	Public               bool    `json:"public"`
 	Protected            bool    `json:"protected"`
 	VideoTitle           string  `json:"videoTitle"`
@@ -32,12 +36,39 @@ type Room struct {
 }
 
 type RoomResponse struct {
-	PublicRoom
-	timestamp float32
+	*PublicRoom
+	Timestamp float64 `json:"timestamp"`
 }
 
-var roomsData = make(map[string]*Room, 1000)
+func NewRoomResponse(room *Room) *RoomResponse {
+	resp := &RoomResponse{}
+	resp.PublicRoom = &room.PublicRoom
+	resp.Timestamp = float64(time.Now().UnixMilli()) / 1000
+	return resp
+}
+
+var rooms = sync.Map{}
 var dataLock = sync.Mutex{}
+
+// utils-------------------------------------------------------------------
+func p(x float64) *float64 {
+	return &x
+}
+
+// ------------------------------------------------------------------------
+
+func QueryRoom(name string) *Room {
+	room, _ := rooms.Load(name)
+	if room == nil {
+		return nil
+	}
+	return room.(*Room)
+}
+
+// TODO localization
+func RenderErrorMessage(w io.Writer, errorMessage string) {
+
+}
 
 func init() {
 
@@ -52,7 +83,7 @@ func init() {
 
 func main() {
 
-	switch strings.TrimSpace(os.Args[0]) {
+	switch strings.TrimSpace(os.Args[1]) {
 	case "debug":
 		panic(http.ListenAndServe("127.0.0.1:5001", nil))
 	case "prod":
@@ -70,19 +101,78 @@ func handleRoomGet(res http.ResponseWriter, req *http.Request) {
 		}
 
 	}()
-	room := req.URL.Query().Get("room")
+	name := req.URL.Query().Get("name")
 	r := render.New()
-	if err := r.JSON(res, 200, room); err != nil {
+	if err := r.JSON(res, 200, NewRoomResponse(QueryRoom(name))); err != nil {
 		panic(err)
 	}
 }
 
 func handleTimestamp(res http.ResponseWriter, req *http.Request) {
+	r := render.New()
+	r.Text(res, 200, "{\"timestamp\":1666624261.2934232}")
+}
 
+func floatParam(req *http.Request, key string, defaultValue *float64) float64 {
+	str := req.URL.Query().Get(key)
+	if str == "" {
+		if defaultValue != nil {
+			return *defaultValue
+		} else {
+			panic(key + " is empty")
+		}
+	}
+	num, err := strconv.ParseFloat(str, 64)
+	if err != nil {
+		if defaultValue != nil {
+			return *defaultValue
+		} else {
+			panic(fmt.Errorf("%s: %s is not float", key, str))
+		}
+	}
+	if math.IsInf(num, 0) || math.IsNaN(num) {
+		if defaultValue != nil {
+			return *defaultValue
+		} else {
+			panic(fmt.Errorf("%s: %s is inf or nan", key, str))
+		}
+	}
+	return num
 }
 
 func handleRoomUpdate(res http.ResponseWriter, req *http.Request) {
+	name := req.URL.Query().Get("name")
+	password := req.URL.Query().Get("password")
+	room := QueryRoom(name)
+	if room == nil {
+		room = &Room{}
+		room.Name = name
+		// TODO hash
+		room.password = password
+		rooms.Store(name, room)
+	} else {
+		if room.password != password {
+			RenderErrorMessage(res, "房名已存在，密码错误")
+			return
+		}
+	}
+	room.PlaybackRate = floatParam(req, "playbackRate", p(1))
+	room.CurrentTime = floatParam(req, "currentTime", nil)
+	room.Paused = req.URL.Query().Get("paused") != "false"
+	room.Url = req.URL.Query().Get("url")
+	room.LastUpdateClientTime = floatParam(req, "lastUpdateClientTime", nil)
+	room.Duration = floatParam(req, "duration", p(1e9))
+	room.LastUpdateServerTime = float64(time.Now().UnixMilli()) / 1000
 
+	// TODO tempUser
+
+	room.Public = false
+	room.Protected = req.URL.Query().Get("protected") == "true"
+	room.VideoTitle = req.URL.Query().Get("videoTitle")
+	r := render.New()
+	if err := r.JSON(res, 200, room); err != nil {
+		panic(err)
+	}
 }
 
 func handleGetVtUserJs(res http.ResponseWriter, req *http.Request) {
