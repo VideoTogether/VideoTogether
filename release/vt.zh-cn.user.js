7475b04ce83bbfda4a7156067408beede880c4ee
m <maggch@outlook.com>
2024-08-07 19:39:03 +0800

    update

--- release/vt.zh-cn.user.js
+++ release/vt.zh-cn.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Video Together 一起看视频
 // @namespace    https://2gether.video/
-// @version      1723028856
+// @version      1723030709
 // @description  Watch video together 一起看视频
 // @author       maggch@outlook.com
 // @match        *://*/*
@@ -1884,7 +1884,7 @@
           <span id="videoTogetherRoleText"></span>
           <span id="memberCount"></span>
         </div>
-        <div id="videoTogetherStatusText" style="height: 22.5px;">已修复轻松分享链接无法访问</div>
+        <div id="videoTogetherStatusText" style="height: 22.5px;"></div>
         <div style="margin-bottom: 10px;">
           <span class="ellipsis" id="videoTogetherRoomNameLabel">房间</span>
           <input id="videoTogetherRoomNameInput" autocomplete="off" placeholder="请输入房间名">
@@ -3166,7 +3166,7 @@
 
             this.activatedVideo = undefined;
             this.tempUser = generateTempUserId();
-            this.version = '1723028856';
+            this.version = '1723030709';
             this.isMain = (window.self == window.top);
             this.UserId = undefined;
 
