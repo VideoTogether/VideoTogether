e93560305c518e6fc7f729c494dc3fd99aed2582
Weijie Zhao <maggch@outlook.com>
2025-01-21 23:35:41 +0800

    Fix pre.js

--- source/chrome/pre.js
+++ source/chrome/pre.js
@@ -1,13 +1,15 @@
-if (document instanceof XMLDocument) {
-    return;
-}
-
-const injectedScript = document.createElement('script');
-injectedScript.src = chrome.runtime.getURL('preInjected.js');
-(document.head || document.documentElement).appendChild(injectedScript);
-sessionStorage.removeItem("VideoTogetherSuperEasyShare");
-chrome.storage.local.get(["SuperEasyShare"], function (result) {
-    if (result["SuperEasyShare"] == true) {
-        sessionStorage.setItem("VideoTogetherSuperEasyShare", 'true');
+(function () {
+    if (document instanceof XMLDocument) {
+        return;
     }
-});
+
+    const injectedScript = document.createElement('script');
+    injectedScript.src = chrome.runtime.getURL('preInjected.js');
+    (document.head || document.documentElement).appendChild(injectedScript);
+    sessionStorage.removeItem("VideoTogetherSuperEasyShare");
+    chrome.storage.local.get(["SuperEasyShare"], function (result) {
+        if (result["SuperEasyShare"] == true) {
+            sessionStorage.setItem("VideoTogetherSuperEasyShare", 'true');
+        }
+    });
+})();
